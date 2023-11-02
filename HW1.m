824b0dd003df0de8436c5353f04e7e8dfe893ae3
Donald Donnelly <ddonnelly12@fordham.edu>
2023-11-01 20:30:00 -0400

    PCA stuff :)

--- HW1.m
+++ HW1.m
@@ -4,7 +4,13 @@ retdata=price2ret(stockdata);
 %plot(retdata)
 
 partition=cvpartition(501,"Holdout",.2);
-idxTrain = training(partition);
-traindata = retdata(idxTrain,:);
-idxNew = test(partition);
-testdata = retdata(idxNew,:);
+traindata = retdata(training(partition),:);
+testdata = retdata(test(partition),:);
+
+[traincoeff,trainscore,trainlatent,traintsquared,trainexplained,trainmu] = pca(traindata);
+[testcoeff,testscore,testlatent,testtsquared,testexplained,testmu] = pca(testdata);
+
+% 1st has the same sign - associated with market movements
+% 2nd, 3rd and 4th - both signs
+%plot(trainexplained)
+%plot(testexplained)
